9f126b3a5737980dc93858f45c70cd7ddbe762b1
Marko Puza <markopuza@gmail.com>
2018-03-04 12:14:21 +0000

    turn fast modular inverse into a function. add test.

--- mathematics/modular_inverse.py
+++ mathematics/modular_inverse.py
@@ -1,15 +1,13 @@
-# Fast modular inverses of 1...n modulo M
-M = 1000000007
-modinv = [-1 for _ in range(n + 1)]
-modinv[1] = 1
-for x in range(2, n + 1):
-    modinv[x] = (-(M//x) * modinv[M%x])%M
-
+def fast_modinv(up_to, M):
+    ''' Fast modular inverses of 1..up_to   modulo M. '''
+    modinv = [-1 for _ in range(up_to + 1)]
+    modinv[1] = 1
+    for x in range(2, up_to + 1):
+        modinv[x] = (-(M//x) * modinv[M%x])%M
+    return modinv
 
 def egcd(a, b):
-    '''
-    Extended Euclidian algorithm
-    '''
+    ''' Extended Euclidian algorithm. '''
     if a == 0:
         return (b, 0, 1)
     else:
@@ -17,9 +15,7 @@ def egcd(a, b):
         return (g, x - (b // a) * y, y)
 
 def modinv(a, m):
-    '''
-    Finds modular inverse modulo m
-    '''
+    ''' Finds modular inverse modulo m. '''
     while a < 0:
         a += m
     g, x, y = egcd(a, m)
@@ -28,8 +24,12 @@ def modinv(a, m):
     else:
         return x % m
 
-def test():
-    print(modinv(2, 5)) # 3
-    print(modinv(3, 10)) # 7
-    print(modinv(12123, 234233249)) # 162724773
-# test()
+if __name__ == "__main__":
+    assert modinv(2, 5) == 3 == fast_modinv(4, 5)[2]
+    assert modinv(3, 10) == 7 == fast_modinv(4, 10)[3]
+    assert modinv(12123, 234233249) == 162724773 == fast_modinv(200000, 234233249)[12123]
+    M = 10**9+7
+    MI = fast_modinv(20000, M)
+    for i in range(300):
+        assert MI[10000+i] == modinv(10000+i, M)
+    print('Tests passed.')
