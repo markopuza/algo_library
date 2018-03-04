e7c3583dfd3e9f6ce6afce4a0031adc4c04323dd
Marko Puza <markopuza@gmail.com>
2018-03-04 11:25:06 +0000

    add test

--- mathematics/factorize_number.py
+++ mathematics/factorize_number.py
@@ -1,11 +1,16 @@
 def primes(n):
-    primfac = []
-    d = 2
+    primfac, d = [], 2
+    #factors repeated
     while d*d <= n:
         while (n % d) == 0:
-            primfac.append(d)  # supposing you want multiple factors repeated
+            primfac.append(d)
             n //= d
         d += 1
     if n > 1:
        primfac.append(n)
     return primfac
+
+if __name__ == "__main__":
+    print('Prime factorizations:')
+    for i in range(50):
+        print('    {:d}  ->  {:s}'.format(i, str(primes(i))))
