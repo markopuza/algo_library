d6a8e3c653a197b61b4c51722cd50a01df89eadf
Marko Puza <markopuza@gmail.com>
2018-03-04 12:17:40 +0000

    add test

--- mathematics/precise_square_root.py
+++ mathematics/precise_square_root.py
@@ -1,4 +1,5 @@
-def precise_square_root(n, precision):
+def precise_square_root(n, precision=100):
+    ''' Calculates square root of number n up to arbitrary (given) precision. '''
     a, b = 5 * n, 5
     while len(str(b)) < precision + 5:
         if a >= b:
@@ -9,6 +10,9 @@ def precise_square_root(n, precision):
             b = 100 * (b // 10) + 5
     return str(b)[:precision]
 
-def test():
-    print(precise_square_root(128937, 100))
-# test()
+if __name__ == "__main__":
+    for n in [128937, 16]:
+        print('    Number {:d}'.format(n))
+        print('        Precise square root: {:s}'.format(precise_square_root(n, 20)))
+        print('        Float square root  : {:.19f}'.format(n ** 0.5))
+        print()
