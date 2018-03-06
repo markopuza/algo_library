81f03b6eb22b9375a3a780a3a7b1ae68ad090b88
Marko Puza <markopuza@gmail.com>
2018-03-06 11:05:11 +0000

    move the z-algorithm. add test.

--- string/z_algorithm.py
+++ string/z_algorithm.py
@@ -1,8 +1,8 @@
 def z_array(s):
     '''
-    Computes z-array of string s
-    Z-arrays, at index i, stores the length of the longest substring of s
-    that starts on i-th place and is also a prefix of s
+        Computes z-array of string s in O(|s|) time.
+        Z-array, at index i, stores the length of the longest substring of s
+        that starts on i-th place and is also a prefix of s
     '''
     n = len(s)
     z = [n] + [0 for _ in range(n - 1)] # z[0] = n
@@ -10,24 +10,19 @@ def z_array(s):
     # stores the last box window
     left, right = 0, 0
     for k in range(1, n):
-        if k > right:
-            # compute naively
+        if k > right: # compute naively
             cnt = 0
             while cnt + k < n and s[cnt] == s[cnt + k]:
                 cnt += 1
             z[k] = cnt
             if cnt > 0:
                 left, right = k, cnt + k - 1
-        else:
-            # we are inside a box, make use of that
+        else: # we are inside a box, make use of that
             p = k - left # corresponding index in a prefix
             right_part_length = right - k + 1
-
-            # use the past results
-            if z[p] < right_part_length:
+            if z[p] < right_part_length: # use the past results
                 z[k] = z[p]
-            else:
-                # compute naively
+            else: # compute naively
                 i = right + 1
                 while i < n and s[i] == s[i - k]:
                     i += 1
@@ -35,8 +30,10 @@ def z_array(s):
                 left, right = k, i - 1
     return z
 
-def test():
+if __name__ == "__main__":
     # https://ivanyu.me/blog/2013/10/15/z-algorithm/
     for s in ['abcxxxabyyy', 'aaaaaa', 'abbbb', 'abcabc', 'abracadabra']:
-        print(z_array(s))
-# test()
+        print('\nString {:s}'.format(s))
+        z = z_array(s)
+        for i in range(len(s)):
+            print(s[i:].ljust(len(s)) + ' and the string start commonly with:  {:s}'.format(s[i:i+z[i]]))
